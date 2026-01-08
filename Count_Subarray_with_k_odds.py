a56132c0192e7591e6e574220c57d737ec05dfaf
Samar Gupta <142289512+samar-12-23@users.noreply.github.com>
2026-01-08 17:05:56 +0530

    Count_Subarray_with_k_odds.py

---
+++ Count_Subarray_with_k_odds.py
@@ -0,0 +1,46 @@
+#GFG #Problem of the day
+#Count Subarray with k odds
+
+#Approach -> Sliding window with Hashmap for Frequency of K odds
+#Time complexity :- O(n)
+#Space Complexity :- O(n)
+
+class Solution:
+
+    def countSubarrays(self, arr, k):
+
+        from collections import defaultdict
+
+        
+
+        freq = defaultdict(int)
+
+        freq[0] = 1 # Base case
+
+        
+
+        odd_count = 0
+
+        count = 0
+
+        
+
+        for num in arr:
+
+            if num % 2 == 1:
+
+                odd_count += 1
+
+            
+
+            if odd_count - k in freq:
+
+                count += freq[odd_count - k]
+
+            
+
+            freq[odd_count] += 1
+
+        
+
+        return count
