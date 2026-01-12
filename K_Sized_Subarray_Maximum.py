5a9223bc4e0e8413fc1bca43e7901e7a7fae9be1
Samar Gupta <142289512+samar-12-23@users.noreply.github.com>
2026-01-12 16:09:23 +0530

    K_Sized_Subarray_Maximum.py

--- K_Sized_Subarray_Maximum.py
+++ K_Sized_Subarray_Maximum.py
@@ -2,8 +2,25 @@
 
 #K Sized Subarray Maximum
 
-#Approach used :- 
+#Approach used :-  Sliding window with Additional space
 
 #Time Complexity :- O(n)
 #Space Complexity :- O(k)
 
+class Solution:
+    def maxOfSubarrays(self, arr, k):
+        # code here
+        q = []  
+        result = []  
+        for i in range(len(arr)):  
+            while q and arr[i] >= arr[q[-1]]:  
+                q.pop()  
+            q.append(i)  
+            # Remove elements out of the current window from the front  
+            while q[0] <= i - k:  
+                q.pop(0)  
+                
+            # Now window size is equal to k. Lets add max element.
+            if i >= k - 1:  
+                result.append(arr[q[0]])  
+        return result
